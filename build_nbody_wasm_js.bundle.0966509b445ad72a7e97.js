c0a1451a81cc0e5e172f102cf2178901b2a78f33
Jeremiah <jpsimms2057@gmail.com>
2022-11-21 15:49:40 -0500

    fixing web app

---
+++ build_nbody_wasm_js.bundle.0966509b445ad72a7e97.js
@@ -0,0 +1,51 @@
+"use strict";
+(self["webpackChunknbody_wasm"] = self["webpackChunknbody_wasm"] || []).push([["build_nbody_wasm_js"],{
+
+/***/ "./build/nbody_wasm.js":
+/*!*****************************!*\
+  !*** ./build/nbody_wasm.js ***!
+  \*****************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.a(module, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _nbody_wasm_bg_wasm__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./nbody_wasm_bg.wasm */ "./build/nbody_wasm_bg.wasm");
+/* harmony import */ var _nbody_wasm_bg_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./nbody_wasm_bg.js */ "./build/nbody_wasm_bg.js");
+var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([_nbody_wasm_bg_wasm__WEBPACK_IMPORTED_MODULE_0__, _nbody_wasm_bg_js__WEBPACK_IMPORTED_MODULE_1__]);
+([_nbody_wasm_bg_wasm__WEBPACK_IMPORTED_MODULE_0__, _nbody_wasm_bg_js__WEBPACK_IMPORTED_MODULE_1__] = __webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__);
+
+
+__webpack_async_result__();
+} catch(e) { __webpack_async_result__(e); } });
+
+/***/ }),
+
+/***/ "./build/nbody_wasm_bg.js":
+/*!********************************!*\
+  !*** ./build/nbody_wasm_bg.js ***!
+  \********************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.a(module, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _nbody_wasm_bg_wasm__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./nbody_wasm_bg.wasm */ "./build/nbody_wasm_bg.wasm");
+var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([_nbody_wasm_bg_wasm__WEBPACK_IMPORTED_MODULE_0__]);
+_nbody_wasm_bg_wasm__WEBPACK_IMPORTED_MODULE_0__ = (__webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__)[0];
+
+__webpack_async_result__();
+} catch(e) { __webpack_async_result__(e); } });
+
+/***/ }),
+
+/***/ "./build/nbody_wasm_bg.wasm":
+/*!**********************************!*\
+  !*** ./build/nbody_wasm_bg.wasm ***!
+  \**********************************/
+/***/ ((module, exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.v(exports, module.id, "5970a236ffaf34affe7c");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVpbGRfbmJvZHlfd2FzbV9qcy5idW5kbGUuMDk2NjUwOWI0NDVhZDcyYTdlOTcuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7O0FBQTZDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbmJvZHlfd2FzbS8uL2J1aWxkL25ib2R5X3dhc20uanMiLCJ3ZWJwYWNrOi8vbmJvZHlfd2FzbS8uL2J1aWxkL25ib2R5X3dhc21fYmcuanMiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0ICogYXMgd2FzbSBmcm9tIFwiLi9uYm9keV93YXNtX2JnLndhc21cIjtcbmV4cG9ydCAqIGZyb20gXCIuL25ib2R5X3dhc21fYmcuanNcIjsiLCJpbXBvcnQgKiBhcyB3YXNtIGZyb20gJy4vbmJvZHlfd2FzbV9iZy53YXNtJztcblxuIl0sIm5hbWVzIjpbIndhc20iXSwic291cmNlUm9vdCI6IiJ9
